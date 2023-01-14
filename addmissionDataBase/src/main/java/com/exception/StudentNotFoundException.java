44128cdd148db470c214a15759a093f81eb4d8ea
Sumeet-16 <sumitdhole1608@gmail.com>
2023-01-14 16:50:51 +0530

    intial

---
+++ addmissionDataBase/src/main/java/com/exception/StudentNotFoundException.java
@@ -0,0 +1,16 @@
+ package com.exception;
+
+public class StudentNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public StudentNotFoundException(String message)
+	{
+		super(message);
+	}
+	
+	public StudentNotFoundException()
+	{
+		
+	}
+
+}
